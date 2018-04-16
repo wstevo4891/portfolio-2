e891ec8917ae594ccfd994b5fb2cdf25cdd1e80a
Eric Stephenson <eric.stephenson@rammount.com>
2018-04-15 19:44:37 -0700

    Turn forms into haml files, fill out inputs

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,8 @@
 # Model for Projects Table
 class Project < ApplicationRecord
+  TECH_OPTIONS = ['Ruby on Rails', 'JavaScript', 'HTML5', 'CSS3', 'SASS',
+                  'NodeJS', 'ReactJS', 'jQuery', 'Bootstrap', 'Heroku'].freeze
+
   has_many :demos, inverse_of: :project
 
   accepts_nested_attributes_for :demos
